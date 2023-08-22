6089ef06ded97b2d95b8e38a05278aaee9bd0ceb
DramixDW <verliefden.romain@gmail.com>
2023-08-22 11:50:54 +0200

    Chapite 4: Template engine
    Faire du rendu de page html dynamiques

--- 04-template-engine/Routers/movie.router.js
+++ 04-template-engine/Routers/movie.router.js
@@ -7,7 +7,20 @@ movieRouter.get('/', (request, response) => {
     // dans notre dossier Views grâce à server.set('views')
     // dans le point d'entrée de notre serveur
     response.render('movies', {
-        message: 'Bienvenue sur ma liste de films :)'
+        message: 'Bienvenue sur ma liste de films :)',
+        title: 'Ma vidéothéque',
+        movies: [
+            {
+                title: 'Forrest Gump',
+            }, {
+                title: 'Alice au pays des merveilles'
+            }
+        ],
+        listElement: function() {
+            return function(text, render) {
+                return '<li>' + render(text) + '</li>';
+            }
+        }
     });
 })
 
